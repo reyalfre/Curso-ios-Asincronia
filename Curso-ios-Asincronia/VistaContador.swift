0c30dbc0ba8b4362677b240fb6582837b1a15418
Alfredo Maldonado <alfredorafael1995@hotmail.com>
2026-02-10 14:48:43 +0100

    vistaContador

---
+++ Curso-ios-Asincronia/VistaContador.swift
@@ -0,0 +1,18 @@
+//
+//  VistaContador.swift
+//  Curso-ios-Asincronia
+//
+//  Created by Equipo 8 on 10/2/26.
+//
+
+import SwiftUI
+
+struct VistaContador: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VistaContador()
+}
