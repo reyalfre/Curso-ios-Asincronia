f13bb9f6d7644ea9ad658e858001c1dcceee5989
Alfredo Maldonado <alfredorafael1995@hotmail.com>
2026-02-10 09:42:09 +0100

    Initial Commit

---
+++ Curso-ios-Asincronia/Curso_ios_AsincroniaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Curso_ios_AsincroniaApp.swift
+//  Curso-ios-Asincronia
+//
+//  Created by Equipo 8 on 10/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Curso_ios_AsincroniaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
